165a756262ce308b8f6dd2f7828cd115f484b97b
Mubarak92 <Mubarak.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-10-20 12:24:30 +0300

    Bonus

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val rows = 5
+    for (i in 1..rows) {
+       for (j in 1..i) {
+        //  for (r in 1..j){
+            print("5*$j=${j*5} ")
+        }
+        println()
+    }}
+
+    //==============================
+
+    /*//val rows = 5
+    for (i in 1..rows) {
+
+
+            print("* ")
+        }
+        println()
+    }
+*/
